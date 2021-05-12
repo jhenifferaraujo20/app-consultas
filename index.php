4da6212843c4b76d16eb6dc495d3468525e0fd88
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2021-05-11 21:46:56 -0300

    Criação do cabecalho.php e rodape.php

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APP Consultas</title>
-    <link rel="stylesheet" href="bootstrap-5/bootstrap.css">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <main class="container">
-        <header>
-            <h1>APP Consultas</h1>
-            <hr>
-        </header>
-        <nav>
-            <a href="#"><img src="img/medico.png" alt="">Cadastro de médicos</a>
-            <a href="#"><img src="img/paciente.png" alt="">Cadastro de pacientes</a>
-            <a href="#"><img src="img/agenda.png" alt="">Agendamento de consultas</a>
-        </nav>
-        <div class="conteudo">
-            <h2>Conteúdo da aplicação</h2>
-        </div>
-        <footer>
-            &copy; Todos os direitos reservados
-        </footer>
-    </main>
-    <script src="bootstrap-5/bootstrap.bundle.js"></script>
-    <script src="js/jquery.min.js"></script>
-</body>
-</html>
+<?php include "cabecalho.php"; ?>
+
+    <div class="conteudo">
+        <h2>Conteúdo da aplicação</h2>
+    </div>
+
+<?php include "rodape.php" ?>
---
+++ rodape.php
@@ -0,0 +1,8 @@
+        <footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+    <script src="bootstrap-5/bootstrap.bundle.js"></script>
+    <script src="js/jquery.min.js"></script>
+</body>
+</html>
